bab99568b3a22c48870b8be5a7f36d323e13735a
Kohsuke Kawaguchi <kk@kohsuke.org>
2007-08-21 16:41:53 +0000

    applied a patch from Pieter Schoenmakers

--- src/org/kohsuke/pdc/ParseDotClasspath.java
+++ src/org/kohsuke/pdc/ParseDotClasspath.java
@@ -1,17 +1,19 @@
 package org.kohsuke.pdc;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.parsers.SAXParserFactory;
-
 import org.xml.sax.Attributes;
 import org.xml.sax.SAXException;
 import org.xml.sax.XMLReader;
 import org.xml.sax.helpers.DefaultHandler;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParserFactory;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 /**
  * Parses Eclipse's &#x2E;classpath files and turns them into
@@ -21,8 +23,13 @@ import org.xml.sax.helpers.DefaultHandler;
  *      Kohsuke Kawaguchi (kk@kohsuke.org)
  */
 public class ParseDotClasspath {
-    
-    private static void usage() {
+
+  private static final String KIND_VAR = "var";
+
+  static Set kinds = new HashSet(Arrays.asList(new String[]{"lib", "output", "src", KIND_VAR}));
+
+  private static void usage ()
+  {
         System.err.println(
             "Usage: java -jar parse-dot-classpath.jar [<options>] <file>*\n"+
             "Reads Eclipse's .classpath file(s), format them as a CLASSPATH\n"+
@@ -33,14 +40,16 @@ public class ParseDotClasspath {
             "if no argument is specified, ./.classpath will be read\n"+
             "\n"+
             "Options:\n"+
-            "  -s <sep>: change the path separator to <sep>.\n" 
+            "  -s <sep>: change the path separator to <sep>.\n" +
+            "Define system properties to use as the prefix of kind=var classpath entries\n" +
+            "for example -DM2_REPO=c:/m2/repository\n"
         );
     }
-    
+
     public static void main(String[] args) throws Exception {
         ClasspathBuilder builder = new ClasspathBuilder();
         List a = new ArrayList();
-        
+
         for( int i=0; i<args.length; i++ ) {
             String arg = args[i].intern();
             if(arg=="-s") {
@@ -53,11 +62,11 @@ public class ParseDotClasspath {
             }
             a.add(arg);
         }
-        
+
         if(a.size()==0)
             // no argument specified. read from the current dir
             parseDotClasspath(new File("./.classpath"),builder);
-        
+
         for( int i=0; i<a.size(); i++ ) {
             File dotCp = new File((String)a.get(i));
             if( dotCp.isDirectory() ) {
@@ -66,10 +75,10 @@ public class ParseDotClasspath {
             }
             parseDotClasspath(dotCp,builder);
         }
-        
+
         // print the result
         System.out.println(builder.getResult());
-        
+
         System.exit(0);
     }
     
@@ -78,9 +87,9 @@ public class ParseDotClasspath {
      * formatted to fit the CLASSPATH variable.
      */
     public static void parseDotClasspath( File dotClasspath, final ClasspathBuilder builder ) throws IOException,SAXException,ParserConfigurationException {
-        // all entries in .classpath are relative to this directory. 
+        // all entries in .classpath are relative to this directory.
         final File baseDir = dotClasspath.getParentFile().getAbsoluteFile();
-        
+
 //        XMLReader parser = XMLReaderFactory.createXMLReader();
         SAXParserFactory spf =SAXParserFactory.newInstance();
         spf.setNamespaceAware(true);
@@ -89,12 +98,19 @@ public class ParseDotClasspath {
             public void startElement(String uri,String localName,String qname,Attributes atts) {
                 if( !localName.equals("classpathentry") )
                     return; // unknown
-                
+
                 String kind = atts.getValue("kind");
-                if(kind.equals("lib") || kind.equals("output") || kind.equals("src")) {
-                    builder.add(absolutize(baseDir,atts.getValue("path")));
+                if (kind != null && kinds.contains(kind)) {
+                    String path = atts.getValue("path");
+                    if (kind.equals(KIND_VAR)) {
+                        int i = path.indexOf('/');
+                        String dir = System.getProperty(path.substring(0, i));
+                        path = dir + '/' + path.substring(i + 1);
+                    }
+
+                    builder.add(absolutize(baseDir, path));
                 }
-                
+
                 String output = atts.getValue("output");
                 if(output!=null) {
                     builder.add(absolutize(baseDir,output));
